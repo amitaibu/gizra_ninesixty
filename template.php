069cc666216d106a74c83f9ec7f10ce8d22f4d3f
Amitaibu <xxx@xxx.com>
2010-02-24 14:08:08 +0200

    On development, when CSS optimization isn't used, it is recommended to use   // Unlimited IE CSS module.

--- template.php
+++ template.php
@@ -1,2 +1,17 @@
 <?php
-// $Id: template.php,v 1.1.2.3 2009/07/18 17:48:55 dvessel Exp $
+// $Id:$
+
+/**
+ * Page preprocess.
+ */
+function gizra_ninesixty_preprocess_page(&$vars) {
+  // On development, when CSS optimization isn't used, it is recommended to use
+  // Unlimited IE CSS module -- http://drupal.org/project/unlimited_css
+  // Following lines, will make sure, it's loading happens.
+  if (module_exists('unlimited_css')) {
+    $vars['styles'] .= unlimited_css_preprocess_page($vars);
+
+    $vars['styles'] .= $vars['conditional_styles'] = variable_get('conditional_styles_' . $GLOBALS['theme'], '');
+  }
+
+}
